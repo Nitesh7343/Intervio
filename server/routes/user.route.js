5b126497959b04efaa9b4827c6524ed5d9867321
Nitesh7343 <niteshsingh7343@gmail.com>
2026-04-13 15:03:39 +0530

    API route created for portfolio page

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express"
 import isAuth from "../middlewares/isAuth.js"
-import { getCurrentUser } from "../controllers/user.controller.js"
+import { getCurrentUser,getPortfolio} from "../controllers/user.controller.js"
+
 
 
 const userRouter = express.Router()
 
 userRouter.get("/current-user",isAuth,getCurrentUser)
+userRouter.get("/portfolio",isAuth,getPortfolio)
 
 export default userRouter
