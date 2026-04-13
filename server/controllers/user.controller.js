2f04f577338142cd709143f0f8e86b84251c404d
Nitesh7343 <niteshsingh7343@gmail.com>
2026-04-13 14:48:43 +0530

    getPortfolio methode implenented

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,45 @@
 import User from "../models/user.model.js"
+import Interview from "../models/interview.model.js"
+import Payment from "../models/payment.model.js"
 
+const getPortfolio = async(req,res) => {
+  try
+  {
+    const userId = req.userId;
+
+    //user
+    const user = await User.findById(userId).select("-password");
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    //interview
+    const interviews = await Interview.find({ userId })
+        .sort({ createdAt: -1 });
+
+    //stats
+    const totalInterviews = interviews.length;
+    const avgScore =
+            interviews.reduce((acc, i) => acc + (i.finalScore || 0), 0) /
+            (totalInterviews || 1);
+
+    return res.status(200).json({
+            user,
+            interviews,
+            stats: {
+                totalInterviews,
+                avgScore: Number(avgScore.toFixed(1))
+            }
+        });
+  } catch(error) 
+   {
+        console.log(error);
+        return res.status(500).json({
+            message: `failed to get portfolio ${error}`
+        });
+    }
+};
 
 export const getCurrentUser = async (req,res) => {
     try {
